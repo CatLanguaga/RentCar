25c4cbbb0ece6c6e44fc8f5702b15d0cbaba8621
Omar <omar@gmail.com>
2024-02-13 12:57:27 -0400

    First Commit

---
+++ src/test/java/com/RentCar/RentaDeVehiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RentCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentaDeVehiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
